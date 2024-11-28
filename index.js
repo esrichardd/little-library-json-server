b3573e399062cddbd9a20f5587e8cc82971795f6
Richard Espinoza <respinoza@galgo.com>
2024-11-28 18:02:04 -0500

    chore: db updated and login created

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
+server.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === "admin@gmail.com" && password === "admin") {
+    res.json({ token: "fake-jwt-token" });
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+});
+
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
