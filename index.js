3f69461f662a71dc262c1a4b5ffb8139cd822bfb
Richard Espinoza <respinoza@galgo.com>
2024-11-28 00:38:31 -0500

    chore: json server

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const path = require("path");
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 4000;
 
 server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-const PORT = 8000;
-
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
+  console.log(`JSON Server is running on port ${PORT}`);
 });
